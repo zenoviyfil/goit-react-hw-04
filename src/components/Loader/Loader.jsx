47e255fe1e484c585653848e74f678e4be0b83e5
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-03-12 21:03:16 +0100

    bug fixed

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -9,7 +9,7 @@ const Loader = () => {
       ariaLabel="color-ring-loading"
       wrapperStyle={{}}
       wrapperClass="color-ring-wrapper"
-      colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+      colors={["#fff", "#fff", "#fff", "#fff", "#fff"]}
     />
   );
 };
