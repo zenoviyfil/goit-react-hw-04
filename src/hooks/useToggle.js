5053d644343a1faeef7413921b9c389f66e52bd5
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-03-13 15:36:21 +0100

    initState add

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-export const useToggle = () => {
-  const [isOpen, setIsOpen] = useState(false);
+export const useToggle = (initialState = false) => {
+  const [isOpen, setIsOpen] = useState(initialState);
 
   const open = () => setIsOpen(true);
 
